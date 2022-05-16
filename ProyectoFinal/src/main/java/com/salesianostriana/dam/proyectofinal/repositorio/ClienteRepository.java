253db00d460585cf8493a5db7834747a60659960
José Luis Hidalgo Navas <jhidalgonavas@gmail.com>
2022-05-16 12:33:41 +0200

    Mucho Avanze

--- ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/repositorio/ClienteRepository.java
+++ ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/repositorio/ClienteRepository.java
@@ -1,9 +1,11 @@
 package com.salesianostriana.dam.proyectofinal.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.salesianostriana.dam.proyectofinal.modelo.Cliente;
 
+@Repository
 public interface ClienteRepository 
 
 extends JpaRepository<Cliente, Long>{
--- ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/repositorio/ProductoRepository.java
+++ ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/repositorio/ProductoRepository.java
@@ -3,8 +3,11 @@ package com.salesianostriana.dam.proyectofinal.repositorio;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.salesianostriana.dam.proyectofinal.modelo.Producto;
 
+@Repository
 public interface ProductoRepository 
 	extends JpaRepository<Producto, Long>{
 	
