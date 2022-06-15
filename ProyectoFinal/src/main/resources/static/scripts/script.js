253a37f97742dc77d94bc5a71fb818bb24814c4c
José Luis Hidalgo Navas <hidalgo.najos22@triana.salesianos.edu>
2022-06-15 12:46:45 +0200

    terminado

--- ProyectoFinal/src/main/resources/static/scripts/script.js
+++ ProyectoFinal/src/main/resources/static/scripts/script.js
@@ -1,24 +1,44 @@
-document.formAgregar.addEventListener("submit",validarform);
+//document.formAgregar.addEventListener("submit",validarform);
+
 
 function validarform(){
-    if(formAgregar.elements.pvp.value > formAgregar.elements.pvp.value.toFixed(2)){
-        formAgregar.elements.pvp.style.borderColor = "#E22E11";
-        formAgregar.elements.pvp.pvp.focus();
-        return true;
-    }else if(formAgregar.elements.pvp.value == ""){
-        formAgregar.elements.pvp.style.borderColor = "#E22E11";
+    let precio = parseFloat(formAgregar.elements.pvp.value);
+
+    let nom = formAgregar.elements.nombre.value;
+
+
+    if(precio > precio.toFixed(2)){
+        formAgregar.elements.pvp.classList.add('border');
+        formAgregar.elements.pvp.classList.add('border-danger');
+        formAgregar.elements.pvp.focus();
+        return false;
+    }else if(precio == "" || precio == 0.0){
+        formAgregar.elements.pvp.classList.add('border');
+        formAgregar.elements.pvp.classList.add('border-danger');
         formAgregar.elements.pvp.focus();   
         return false;
     }
 
-    let max = 100;
+    let max = 150;
 
-    if(document.forms.formAgregar.getElementById("nombre").value > max){
-        formAgregar.elements.pvp.style.borderColor = "#E22E11";
-        formAgregar.elements.nombre.focus()
+    if(nom.length > max){
+        formAgregar.elements.nombre.classList.add('border');
+        formAgregar.elements.nombre.classList.add('border-danger');
+        nom.focus();
+        return false;
+    }else if(nom == ""){
+        formAgregar.elements.nombre.classList.add('border');
+        formAgregar.elements.nombre.classList.add('border-danger');
+        nom.focus()
         return false;
     }
-
 }
+
+
+
+
+
+
+
 
 
